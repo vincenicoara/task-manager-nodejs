aeb290e5d3ef5e9f98b00f678cc727e4bd453c75
vincenicoara <vincenicoara@gmail.com>
2019-10-10 10:46:02 -0700

    added GET and PUT calls for Tasks and Users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,57 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if (!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        String
-    },
-    completed: {
-        Boolean
-    }
-})
+module.exports = mongoose
 
 
-
-const vince = new User({
-    name: 'Mike      ',
-    email: 'miKELS@yahoo.com    '
-})
-
-vince.save().then(() => {
-    console.log(vince)
-}).catch((err)=>{
-    console.log(err)
-})
